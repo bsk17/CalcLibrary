bd67a09195a11ffa0a1aa5c730b342758591d9e8
Kurien <908001@cognizant.com>
2021-05-06 16:28:22 +0530

    Add project files.

---
+++ CalcLibraryTest/CalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcLibrary;
+
+namespace CalcLibraryTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AdditionTest()
+        {
+            //Arrange
+            SimpleCalculator calc = new SimpleCalculator();
+
+            //Act
+            double result = calc.Addition(1.0f,2.0f);
+
+            //Assert
+            Assert.AreEqual(3.0,result);
+        }
+
+        [TestMethod]
+        public void SubstractionTest()
+        {
+            //Arrange
+            SimpleCalculator calc = new SimpleCalculator();
+
+            //Act
+            double result = calc.Subtraction(4.0f, 2.0f);
+
+            //Assert
+            Assert.AreEqual(2.0, result);
+        }
+
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            //Arrange
+            SimpleCalculator calc = new SimpleCalculator();
+
+            //Act
+            double result = calc.Multiplication(3.0f,4.0f);
+
+            //Assert
+            Assert.AreEqual(12.0,result);
+        }
+    }
+}
---
+++ CalcLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6b550cb-172c-40b3-93a7-bbf02a3b5e33")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
